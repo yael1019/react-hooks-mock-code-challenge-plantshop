fb0930af9cfe0de9ea5a037c67da61d0bf3773c7
Yael Harosh <yaelharosh01@gmail.com>
2023-03-22 12:00:23 -0400

    patch works

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,31 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function PlantCard({ plant, patchPrice, handleDelete }) {
-// SET STATE FOR OUT OF STOCK PLANTS 
+  // SET STATE FOR OUT OF STOCK PLANTS 
   const [outOfStock, setOutOfStock] = useState(false);
-// SET STATE FOR PRICE CHANGE 
+  // SET STATE FOR PRICE CHANGE 
   const [price, setPrice] = useState('');
-// FUNCTION TO TOGGLE OUTOFSTOCK
+  // FUNCTION TO TOGGLE OUTOFSTOCK
   const handleClick = () => setOutOfStock(!outOfStock)
-// FUNCTION TO RESET PRICE STATE
+  // FUNCTION TO RESET PRICE STATE
   const handlePriceChange = e => {
-    console.log(`befor: ${price}`)
-    setPrice(price => price = e.target.value)
-    console.log(`after: ${price}`)
-    patchPrice(plant, price)
+    setPrice(price2 => price2 = e.target.value)
   }
+  // useEffect(() => {
+  //   patchPrice(plant, price)
+  // }, [price])
   return (
     <li className="card">
-      <img src={ plant.image } alt={ plant.name } />
-      <h4>{ plant.name }</h4>
-      <p>Price: { plant.price }</p>
-      <input type="text" placeholder="New Price" name='price' value={ price } onChange={ handlePriceChange } />
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: {plant.price}</p>
+      <form onSubmit={() => patchPrice(plant, price)}>
+        <input type="text" placeholder="New Price" name='price' value={price} onChange={handlePriceChange} />
+      </form>
       {outOfStock ? (
-        <button onClick={ handleClick }>Out of Stock</button>
+        <button onClick={handleClick}>Out of Stock</button>
       ) : (
-        <button className="primary" onClick={ handleClick }>In Stock</button>
+        <button className="primary" onClick={handleClick}>In Stock</button>
       )}
-      <button onClick={ () => handleDelete(plant.id) }>Delete Plant</button>
+      <button onClick={() => handleDelete(plant.id)}>Delete Plant</button>
     </li>
   );
 }
