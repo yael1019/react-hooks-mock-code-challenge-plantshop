4e58fc7271ef5088d31587fcc49892eb3ef01740
Yael Harosh <yaelharosh01@gmail.com>
2023-03-22 11:14:11 -0400

    everything but patch works

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,20 +1,31 @@
 import React, { useState } from "react";
 
-function PlantCard({ plant }) {
+function PlantCard({ plant, patchPrice, handleDelete }) {
 // SET STATE FOR OUT OF STOCK PLANTS 
   const [outOfStock, setOutOfStock] = useState(false);
+// SET STATE FOR PRICE CHANGE 
+  const [price, setPrice] = useState('');
 // FUNCTION TO TOGGLE OUTOFSTOCK
   const handleClick = () => setOutOfStock(!outOfStock)
+// FUNCTION TO RESET PRICE STATE
+  const handlePriceChange = e => {
+    console.log(`befor: ${price}`)
+    setPrice(price => price = e.target.value)
+    console.log(`after: ${price}`)
+    patchPrice(plant, price)
+  }
   return (
     <li className="card">
       <img src={ plant.image } alt={ plant.name } />
       <h4>{ plant.name }</h4>
       <p>Price: { plant.price }</p>
+      <input type="text" placeholder="New Price" name='price' value={ price } onChange={ handlePriceChange } />
       {outOfStock ? (
         <button onClick={ handleClick }>Out of Stock</button>
       ) : (
         <button className="primary" onClick={ handleClick }>In Stock</button>
       )}
+      <button onClick={ () => handleDelete(plant.id) }>Delete Plant</button>
     </li>
   );
 }
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -8,10 +8,33 @@ function PlantList({ plants, setPlants, search }) {
       .then(resp => resp.json())
       .then(data => setPlants(data))
   }, [])
+// FUNCTION TO PATCH NEW PRICE
+  const patchPrice = (plantA, price) => {
+    fetch(`http://localhost:6001/plants/${plantA.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accepts': 'application/json'
+      },
+      body: JSON.stringify({price: price})
+    })
+      .then(resp => resp.json())
+      .then(data => {
+        setPlants(plants.map(plant => plant.id === plantA.id ? data : plant))
+      })
+  }
+// FUNCTION TO DELETE PLANTS  
+  const handleDelete = (plantID) => {
+    fetch(`http://localhost:6001/plants/${plantID}`, {
+      method: 'DELETE'
+    })
+      .then(resp => resp.json())
+      .then(() => setPlants(plants.filter(plant => plant.id !== plantID)))
+  }
 // FILTER THROUGH PLANTS ARRAY ACCORDING TO SEARCH
   const filteredPlants = plants.filter(plant => plant.name.toLowerCase().includes(search.toLowerCase()))
 // MAP THROUGH PLANTS AND MAKE A PLANT CARD FOR EACH PLANT
-  const displayPlants = filteredPlants.map(plant => <PlantCard key={ plant.id } plant={ plant } />)
+  const displayPlants = filteredPlants.map(plant => <PlantCard key={ plant.id } plant={ plant } patchPrice={ patchPrice } handleDelete={ handleDelete } />)
   return (
     <ul className="cards">{ displayPlants }</ul>
   );
